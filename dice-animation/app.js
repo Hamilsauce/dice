2b86a009ce93368800a25a6189de4876fb7757f6
hamilsauce <jake@seahag.onmicrosoft.com>
2020-05-27 01:18:37 -0500

    anime

--- dice-animation/app.js
+++ dice-animation/app.js
@@ -1,10 +1,26 @@
-function rollDice() {
-  const dice = [...document.querySelectorAll(".die-list")];
-  dice.forEach((die, index) => {
-    toggleClasses(die);
+const selected = []
+const kept = [];
 
+
+function rollDice() {
+  const rollDisplay = document.querySelector(`.rollDisplay`)
+  const dice = [...document.querySelectorAll(".die-list")]
+    .filter(die => {
+      return die.dataset.kept = 'false';
+      console.log('filter die: ');
+    })
+  console.log(dice);
+  dice.forEach((die, index) => {
+    registerEventListeners(die)
+    toggleClasses(die);
     die.dataset.roll = getRandomNumber(1, 6);
-    document.querySelector(`.dieValue${index + 1}`).textContent = die.dataset.roll;
+
+    let el = document.createElement('div');
+    let dieClass = `dieValue${die.dataset.roll}`
+    el.textContent = die.dataset.roll;
+
+    el.classList.add(dieClass)
+    rollDisplay.appendChild(el);
   });
 }
 
@@ -19,4 +35,85 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-document.getElementById("roll-button").addEventListener("click", rollDice);
+const registerEventListeners = die => {
+  const rollDisplay = document.querySelector(`.rollDisplay`)
+  let el = document.querySelector(`.${this.id}Value`);
+
+
+  // dice.forEach(die => {
+
+  die.addEventListener('click', e => {
+    console.log('click2');
+    // let keptDice = this.diceSet.getKeptDice();
+    // let selectedDice = this.diceSet.selectedDice();
+    // let keptVals = rollDisplay.textContent	
+    // let keptCheck = keptDice.every(d => {
+    //   return d.value === this.value;
+    // })
+    // let selectedCheck = selectedDice.every(d => {
+    //   return d.value === this.value;
+    // })
+
+    die.classList.toggle('selected')
+    if (die.classList.contains('selected')) {
+      selected.push(die.dataset.roll)
+      die.dataset.selected = 'true';
+    } else {
+      let popped = selected.pop()
+      die.dataset.selected = 'false';
+      console.log(popped);
+    }
+    console.log(die);
+    console.log(selected);
+    /*   
+       
+         if (keptCheck === false || selectedCheck === false && selectedDice.length !== 0) {
+           this.selected = false;
+           die.classList.remove('selected');
+
+           console.log('must pick previously kept die val');
+           return;
+         } else if (this.selected) {
+           this.selected = false;
+           die.classList.remove('selected');
+         } else {
+           this.selected = true;
+           die.classList.add('selected');
+         }*/
+  })
+  // })
+}
+const keepDice = () => {
+  const dice = [...document.querySelectorAll(".die-list")];
+  let arr = [];
+
+  let sel = dice.filter(die => {
+    console.log(die.dataset.selected === 'true');
+    return die.classList.contains('selected') == true;
+  })
+  let unsel = dice.filter(die => {
+    console.log(die.dataset.selected === false);
+    return die.classList.contains('selected') == false;
+  })
+  sel.forEach(die => {
+    // die.classList.remove('selected')
+    die.classList.add('kept')
+    die.dataset.kept = 'true';
+  })
+  console.log('sel');
+  console.log(sel);
+  console.log('unsel');
+  console.log(unsel);
+ let kep = dice.filter(die => {
+   console.log(die.dataset.selected === false);
+   return die.classList.contains('kept') == true;
+ })
+  console.log('kep');
+  console.log(kep);
+}
+
+document.getElementById("roll-button")
+  .addEventListener("click", e => {
+    keepDice()
+    rollDice()
+  });
