86dec38bcf4312127e021ad3e30c2fcdd84b9822
hamilsauce <jake@seahag.onmicrosoft.com>
2021-03-16 16:30:24 -0700

    history page sticky heads, row/col hilites

--- history-page.js
+++ history-page.js
@@ -6,9 +6,23 @@ import {
 storeFactory()
 store.getLocalStorage('diceGameHistory')
 store.reIndexGames()
-const firstGame = store.state.gameHistory[[0]]
 
-const gameDate = new Date(firstGame.gameDate)
+// const firstGame = store.state.gameHistory[[0]]
+
+// const gameDate = new Date(firstGame.gameDate)
+const headerRow = document.querySelector('.sticky-container')
+console.log(headerRow);
+const headerPos = headerRow.offsetTop;
+
+const stickyHeader = () => {
+	console.log(headerPos);
+	if (window.pageYOffset > headerPos) {
+		headerRow.classList.add("sticky");
+	} else {
+		headerRow.classList.remove("sticky");
+	}
+}
+
 
 const getFormattedDate = dateString => {
 	const day = new Date(dateString).getDate()
@@ -27,7 +41,6 @@ const buildRow = game => {
 	const dateField = document.createElement('td')
 	dateField.classList.add('table-field');
 	dateField.dataset.id = game.id;
-
 	dateField.dataset.column = 1;
 	dateField.innerText = getFormattedDate(game.gameDate)
 	row.appendChild(dateField)
@@ -36,7 +49,6 @@ const buildRow = game => {
 	nameField.classList.add('table-field');
 	nameField.dataset.column = 2;
 	nameField.dataset.id = game.id;
-
 	nameField.innerText = game.gameName
 	row.appendChild(nameField)
 
@@ -44,76 +56,74 @@ const buildRow = game => {
 	winnerField.classList.add('table-field');
 	winnerField.dataset.column = 3;
 	winnerField.dataset.id = game.id;
-	
 	if (typeof game.winner == 'string') {
 		winnerField.innerText = game.winner
 		row.appendChild(winnerField)
-
 	} else {
-		// if (game.winner.name) {
 		winnerField.innerText = game.winner.name ? game.winner.name : game.winner.id
 		row.appendChild(winnerField)
-
-		// } else {
-
-		// }
 	}
-
 	return row
-
 }
 
 const buildTable = () => {
 	const games = store.state.gameHistory
-	console.log(store.state.gameHistory);
 	const body = document.querySelector('.table-body')
-	console.log();
 	games.forEach(game => {
 		const row = buildRow(game)
 		body.appendChild(row)
 	})
-
 }
-console.log(store.state.gameHistory);
 buildTable();
 
-console.log(getFormattedDate(firstGame.gameDate));
+let colNum = null;
+let rowNum = null;
 
+//Hilight col when header clicked
 document.querySelectorAll('.table-header')
 	.forEach(head => {
 		head.addEventListener('click', e => {
 			const col = e.target.dataset.column;
-
+			colNum = col;
 			const fields = document.querySelectorAll('.table-field')
+
 			fields.forEach(field => {
 				if (field.dataset.column == col) {
 					field.classList.toggle('col-selected')
 				} else {
 					field.classList.remove('col-selected')
-
 				}
 
+				if (field.dataset.id == rowNum && field.dataset.column == colNum) {
+					field.classList.add('intersect-field')
+				} else {
+					field.classList.remove('intersect-field')
+				}
 			})
-
-
 		})
 	})
+
+//highlight row when row cell clicked
 document.querySelectorAll('.table-field')
 	.forEach(field => {
 		field.addEventListener('click', e => {
 			const id = e.target.parentElement.dataset.id;
-
+			const rowNum = id
 			const fields = document.querySelectorAll('.table-field')
+
 			fields.forEach(field => {
 				if (field.dataset.id == id) {
 					field.classList.toggle('row-selected')
 				} else {
 					field.classList.remove('row-selected')
-
 				}
-
+				if (field.dataset.id == rowNum && field.dataset.column == colNum) {
+					field.classList.add('intersect-field')
+				} else {
+					field.classList.remove('intersect-field')
+				}
 			})
-
-
 		})
 	})
+
+window.onscroll = function() { stickyHeader() };
