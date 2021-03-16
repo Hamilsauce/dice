a333a52a4c6546485a18cfde575a2c6f38c07047
hamilsauce <jake@seahag.onmicrosoft.com>
2021-03-16 04:57:44 -0700

    integrated histpry page

---
+++ app.js
@@ -0,0 +1,100 @@
+Vue.component('editable-text', {
+	data: function() {
+		return {
+			message: "Change me"
+		}
+	},
+	template: `
+	<div v-for="(el, i) in items" :key="i">
+					<p>Message is: {{ message }}</p>
+					<p>{{ el.name }} {{ i }}</p>
+					<input v-model="message" placeholder="edit me" />
+				</div>
+		<div>
+			<p>Message is: {{ message }}</p>
+			<input v-model="message" placeholder="edit me" />
+		</div>`
+})
+
+Vue.component('table-row', {
+	props: ['game'],
+	data: function() {
+		return {
+			message: "table row"
+		}
+	},
+	template: `
+		<tr class="table-row" data-id="game.id">
+			<td class="column1 game-id-field" data-column="1">{{ game.id }}</td>
+			<td class="column2 game-name-field" data-column="2">{{ game.name }}</td>
+			<td class="column3 game-winner-field" data-column="3">{{ game.winner }}</td>
+		</tr>`
+})
+Vue.component('game-table', {
+	components: {},
+	props: ['games'],
+	data: function() {
+		return {
+			message: "Change me"
+		}
+	},
+	template: `
+		<table class="game-table">
+			<thead>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Winner</th>
+					</tr>
+				</thead> 
+				<tbody>
+					<table-row v-for="game in games" :game="game" :key="game.id"></table-row>
+				</tbody>
+		</table>`
+})
+
+
+var study = new Vue({
+	el: "#vue-app",
+	data: {
+		heading: "Vue.js",
+		text: "Happiness makes up in height what it lacks in length.",
+		games: [],
+		games1: [
+			{
+				id: 1,
+				name: "horses",
+				winner: "bob"
+			  },
+			{
+				id: 2,
+				name: "horses",
+				winner: "sally"
+			  },
+			{
+				id: 3,
+				name: "threes",
+				winner: "tom"
+			  },
+			{
+				id: 4,
+				name: "scc",
+				winner: "jake"
+			  }
+			]
+
+
+	},
+	methods: {
+		getGames() {
+			const gameString = localStorage.getItem('diceGameHistory')
+			this.games = JSON.stringify(gameString)
+			console.log(this.games);
+		}
+	},
+	mounted() {
+		this.getGames()
+	}
+});
+
+console.log(study);
---
+++ history-page.js
@@ -0,0 +1,119 @@
+import {
+	store,
+	storeFactory
+} from './js/store.js'
+
+storeFactory()
+store.getLocalStorage('diceGameHistory')
+
+const firstGame = store.state.gameHistory[[0]]
+
+const gameDate = new Date(firstGame.gameDate)
+
+const getFormattedDate = dateString => {
+	const day = new Date(dateString).getDate()
+	const mo = new Date(dateString).getMonth() + 1
+	const yr = new Date(dateString).getFullYear();
+	const formattedDate = `${mo}/${day}/${yr}`;
+
+	return formattedDate;
+}
+
+const buildRow = game => {
+	const row = document.createElement('tr')
+	row.classList.add('table-row');
+	row.dataset.id = game.id
+
+	const dateField = document.createElement('td')
+	dateField.classList.add('table-field');
+	dateField.dataset.id = game.id;
+
+	dateField.dataset.column = 1;
+	dateField.innerText = getFormattedDate(game.gameDate)
+	row.appendChild(dateField)
+
+	const nameField = document.createElement('td')
+	nameField.classList.add('table-field');
+	nameField.dataset.column = 2;
+	nameField.dataset.id = game.id;
+
+	nameField.innerText = game.gameName
+	row.appendChild(nameField)
+
+	const winnerField = document.createElement('td')
+	winnerField.classList.add('table-field');
+	winnerField.dataset.column = 3;
+	winnerField.dataset.id = game.id;
+	
+	if (typeof game.winner == 'string') {
+		winnerField.innerText = game.winner
+		row.appendChild(winnerField)
+
+	} else {
+		// if (game.winner.name) {
+		winnerField.innerText = game.winner.name ? game.winner.name : game.winner.id
+		row.appendChild(winnerField)
+
+		// } else {
+
+		// }
+	}
+
+	return row
+
+}
+
+const buildTable = () => {
+	const games = store.state.gameHistory
+	console.log(store.state.gameHistory);
+	const body = document.querySelector('.table-body')
+	console.log();
+	games.forEach(game => {
+		const row = buildRow(game)
+		body.appendChild(row)
+	})
+
+}
+console.log(store.state.gameHistory);
+buildTable();
+
+console.log(getFormattedDate(firstGame.gameDate));
+
+document.querySelectorAll('.table-header')
+	.forEach(head => {
+		head.addEventListener('click', e => {
+			const col = e.target.dataset.column;
+
+			const fields = document.querySelectorAll('.table-field')
+			fields.forEach(field => {
+				if (field.dataset.column == col) {
+					field.classList.toggle('col-selected')
+				} else {
+					field.classList.remove('col-selected')
+
+				}
+
+			})
+
+
+		})
+	})
+document.querySelectorAll('.table-field')
+	.forEach(field => {
+		field.addEventListener('click', e => {
+			const id = e.target.parentElement.dataset.id;
+
+			const fields = document.querySelectorAll('.table-field')
+			fields.forEach(field => {
+				if (field.dataset.id == id) {
+					field.classList.toggle('row-selected')
+				} else {
+					field.classList.remove('row-selected')
+
+				}
+
+			})
+
+
+		})
+	})
